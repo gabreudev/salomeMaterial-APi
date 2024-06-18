63773e3b7910f6ba1fa79e4a1da9de6a137ace47
gabriel <biel0alves0@gmail.com>
2024-06-18 10:07:45 -0300

    initial commit

---
+++ src/main/java/com/salomeMaterial_api/salomeMaterial/SalomeMaterialApplication.java
@@ -0,0 +1,13 @@
+package com.salomeMaterial_api.salomeMaterial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalomeMaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalomeMaterialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salomeMaterial_api/salomeMaterial/SalomeMaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salomeMaterial_api.salomeMaterial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalomeMaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
