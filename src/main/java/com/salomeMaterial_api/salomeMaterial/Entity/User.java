d15a296ce51604eec3fb25ef202fab69f2104cdd
gabriel <biel0alves0@gmail.com>
2024-06-18 13:59:45 -0300

    feat: add user

---
+++ src/main/java/com/salomeMaterial_api/salomeMaterial/Entity/User.java
@@ -0,0 +1,19 @@
+package com.salomeMaterial_api.salomeMaterial.Entity;
+
+import lombok.Getter;
+
+
+public class User {
+
+    private String Cpf;
+
+    private String email;
+
+    private String nome;
+
+    public String getEmail() {
+        return email;
+
+        }
+}
+
