e095b611510b1457f53f920fe7f2a790bdabf491
gabriel <biel0alves0@gmail.com>
2024-06-18 13:58:19 -0300

    feat: add and config mailService

---
+++ src/main/java/com/salomeMaterial_api/salomeMaterial/Service/mailService.java
@@ -0,0 +1,38 @@
+package com.salomeMaterial_api.salomeMaterial.Service;
+
+
+import com.salomeMaterial_api.salomeMaterial.Entity.User;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class mailService {
+
+    @Autowired
+    JavaMailSender emailSender;
+
+
+    public void sendMaterialEmail(User user) throws MessagingException, UnsupportedEncodingException {
+    String toAdress = user.getEmail();
+    String fromAdress = "${EMAIL.SENDER}";
+    String senderName = "SalomeStudies";
+    String subject = "Material da Salome Studies";
+
+    String content = "PAGINA HTML COM O MATERIAL";
+
+    MimeMessage mimeMessage = emailSender.createMimeMessage();
+    MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+    messageHelper.setFrom(fromAdress,senderName);
+    messageHelper.setTo(toAdress);
+    messageHelper.setSubject(subject);
+
+    messageHelper.setText(content);
+    emailSender.send(mimeMessage);
+    }
+}
