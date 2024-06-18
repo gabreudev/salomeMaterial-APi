2304f98cc28caa6564bfc44ce2148a0c8f595a56
gabriel <biel0alves0@gmail.com>
2024-06-18 16:45:43 -0300

    feat: add controller mail

--- src/main/java/com/salomeMaterial_api/salomeMaterial/Service/MailService.java
+++ src/main/java/com/salomeMaterial_api/salomeMaterial/Service/MailService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.io.UnsupportedEncodingException;
 
 @Service
-public class mailService {
+public class MailService {
 
     @Autowired
     JavaMailSender emailSender;
