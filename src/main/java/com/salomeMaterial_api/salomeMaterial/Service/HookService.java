915a69fadc2d8391c5a79e88b0f246ce71e699c7
gabriel <biel0alves0@gmail.com>
2024-06-22 20:52:28 -0300

    feat: add listhook metod

--- src/main/java/com/salomeMaterial_api/salomeMaterial/Service/HookService.java
+++ src/main/java/com/salomeMaterial_api/salomeMaterial/Service/HookService.java
@@ -4,22 +4,28 @@ import br.com.efi.efisdk.EfiPay;
 import br.com.efi.efisdk.exceptions.EfiPayException;
 import com.salomeMaterial_api.salomeMaterial.Pix.Credentials;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
+
+
 @Service
 public class HookService {
+
+
+    @Value("${CLIENT_ID}")
+    private String clientId;
+
+    @Value("${CLIENT_SECRET}")
+    private String clientSecret;
 
     public void configHook(){
 
         Credentials credentials = new Credentials();
-
-        JSONObject options = new JSONObject();
-        options.put("client_id", credentials.getClientId());
-        options.put("client_secret", credentials.getClientSecret());
-        options.put("certificate", credentials.getCertificate());
-        options.put("sandbox", credentials.isSandbox());
+        
+        JSONObject options = configuringJsonObject();
 
         options.put("x-skip-mtls-checking", "true");
 
@@ -27,7 +33,7 @@ public class HookService {
         params.put("chave", "2d9c6bfd-d19e-4123-8a35-c2f0caac55db");
 
         JSONObject body = new JSONObject();
-        body.put("webhookUrl", "https://seudominio.com.br/webhook/");
+        body.put("webhookUrl", "https://5e94-45-170-222-201.ngrok-free.app/webhook/");
 
         try {
             EfiPay efi = new EfiPay(options);
@@ -42,5 +48,39 @@ public class HookService {
             System.out.println(e.getMessage());
         }
     }
+    public void listWebHooks(){
 
+        Credentials credentials = new Credentials();
+
+
+        JSONObject options = configuringJsonObject();
+        
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("chave", "2d9c6bfd-d19e-4123-8a35-c2f0caac55db");
+
+        try {
+            EfiPay efi= new EfiPay(options);
+            JSONObject response = efi.call("pixDetailWebhook", params, new JSONObject());
+            System.out.println(response);
+        }catch (EfiPayException e){
+            System.out.println(e.getError());
+            System.out.println(e.getErrorDescription());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private JSONObject configuringJsonObject(){
+        Credentials credentials = new Credentials();
+
+        JSONObject options = new JSONObject();
+        options.put("client_id", clientId);
+        options.put("client_secret", clientSecret);
+        options.put("certificate", credentials.getCertificate());
+        options.put("sandbox", credentials.isSandbox());
+
+        return options;
+    }
 }
+
