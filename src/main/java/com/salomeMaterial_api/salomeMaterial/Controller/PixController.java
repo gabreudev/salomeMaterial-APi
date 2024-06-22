8c680ef4677dd870c1e98366c53080b7a79b770f
gabriel <biel0alves0@gmail.com>
2024-06-22 14:19:23 -0300

    feat: add Pixcontroller

---
+++ src/main/java/com/salomeMaterial_api/salomeMaterial/Controller/PixController.java
@@ -0,0 +1,36 @@
+package com.salomeMaterial_api.salomeMaterial.Controller;
+
+
+import com.salomeMaterial_api.salomeMaterial.Entity.PixChargeRequest;
+import com.salomeMaterial_api.salomeMaterial.Service.PixService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/pix")
+public class PixController {
+
+    @Autowired
+    private PixService pixService;
+
+    @GetMapping
+    public ResponseEntity pixCreateEVP(){
+
+        JSONObject response = this.pixService.pixCreateEVP();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response.toString());
+    }
+
+    @PostMapping
+    public ResponseEntity pixCreateCharge(@RequestBody PixChargeRequest pixChargeRequest){
+        JSONObject response = this.pixService.pixCreateCharge(pixChargeRequest);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response.toString());
+    }
+}
